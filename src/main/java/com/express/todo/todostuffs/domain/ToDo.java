a28cf01dfed12f0f7ec408f330933072d69d3ef1
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-26 19:04:21 +0100

    created model for todo

--- src/main/java/com/express/todo/todostuffs/domain/ToDo.java
+++ src/main/java/com/express/todo/todostuffs/domain/ToDo.java
@@ -1,4 +1,31 @@
 package com.express.todo.todostuffs.domain;
 
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
 public class ToDo {
+    @NotNull
+    private String id;
+    @NotNull
+    private String description;
+    private LocalDateTime created;
+    private LocalDateTime modifieed;
+    private boolean completed;
+
+    public ToDo() {
+        LocalDateTime date = LocalDateTime.now();
+        this.id = UUID.randomUUID().toString();
+        this.created = date;
+        this.modifieed = date;
+
+         }
+    public ToDo(String description) {
+        this();
+        this.description = description;
+    }
 }
