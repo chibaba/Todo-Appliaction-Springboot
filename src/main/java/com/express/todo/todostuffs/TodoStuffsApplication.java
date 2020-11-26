333a97fbaacdf7d563cabb05c6fd314864c75ef0
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-26 18:44:34 +0100

    setting up todo applications

---
+++ src/main/java/com/express/todo/todostuffs/TodoStuffsApplication.java
@@ -0,0 +1,13 @@
+package com.express.todo.todostuffs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoStuffsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoStuffsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/express/todo/todostuffs/TodoStuffsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.express.todo.todostuffs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoStuffsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
