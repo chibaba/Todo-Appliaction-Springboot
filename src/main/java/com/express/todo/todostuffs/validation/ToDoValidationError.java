9485a8b7d690712aa4f957e3376b3b580015fc35
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-26 21:16:06 +0100

    add more validation errors

--- src/main/java/com/express/todo/todostuffs/validation/ToDoValidationError.java
+++ src/main/java/com/express/todo/todostuffs/validation/ToDoValidationError.java
@@ -1,4 +1,19 @@
 package com.express.todo.todostuffs.validation;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToDoValidationError {
+    @JsonInclude(Include.NON_EMPTY)
+    private List<String> errors = new ArrayList<>();
+
+    private final String errorMessage;
+
+    public ToDoValidationError(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
 }
