24446be612b8e467f29d1385bd3faf44816be29d
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-26 21:07:48 +0100

    a template repository

--- src/main/java/com/express/todo/todostuffs/repository/ToDoRepository.java
+++ src/main/java/com/express/todo/todostuffs/repository/ToDoRepository.java
@@ -4,8 +4,11 @@ import com.express.todo.todostuffs.domain.ToDo;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class ToDoRepository implements CommonRepository <ToDo>{
 
@@ -28,4 +31,22 @@ public class ToDoRepository implements CommonRepository <ToDo>{
         domains.forEach(this::save);
         return  findAll();
     }
+    @Override
+    public void delete(ToDo domain) {
+        toDos.remove(domain.getId());
+    }
+
+    @Override
+    public ToDo findById(String id){
+        return toDos.get(id);
+    }
+    @Override
+    public Iterable<ToDo> findAll() {
+        return toDos.entrySet().stream().sorted(entryComparator).
+                map(Map.Entry::getValue).collect(Collectors.toList());
+    }
+    private Comparator<Map.Entry<String, ToDo>> entryComparator = (Map.
+            Entry<String, ToDo> o1, Map.Entry<String, ToDo> o2) -> {
+        return o1.getValue().getCreated().compareTo(o2.getValue().getCreated());
+    };
 }
